044c8fd6a2c4fdd409d70ad5959327bbca677a24
Jean Ferreira <jeansouzza@gmail.com>
2022-07-20 21:46:05 -0300

    Update BACKEND_URL to a standing one

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 const BACKEND_URL = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://tibiaflix-react.herokuapp.com';
+  : 'https://kurt-tibiaflix.herokuapp.com';
+
+// Old url = https://tibiaflix-react.herokuapp.com
 
 export default { BACKEND_URL };
